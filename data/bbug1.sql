42643818fbda1e766314deefaeb2f4cb4fcc3cbb
bookug <zengli@bookug.cc>
2017-11-16 16:19:29 +0800

    style: change datasets and queries

--- data/bbug1.sql
+++
@@ -1,1 +0,0 @@
-select ?subject ?predict ?object WHERE { ?subject <http://www.founder.20.link:52> ?object; ?predict ?object .  }
--- data/exist.sql
+++
@@ -1,1 +0,0 @@
-select ?s where {  <http://www.founder/100> <http://www.founder.20.attr:dmID> "22". }
