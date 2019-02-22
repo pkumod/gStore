c50779d527bbb3843bf3cf6bfa4c062915b7d087
YuqqiZhou <41812724+YuqqiZhou@users.noreply.github.com>
2019-02-22 19:43:54 +0800

    change fread to mmap

--- StringIndex/StringIndex.h
+++ StringIndex/StringIndex.h
@@ -15,15 +15,23 @@
 class StringIndexFile
 {
 	public:
-		static const long MAX_BLOCK_SIZE = 10000000;
+		//static const long MAX_BLOCK_SIZE = 10000000;
+		static const long MAX_BLOCK_SIZE = 2097152;
 		enum StringIndexFileType {Entity, Literal, Predicate};
+		void SetTrie(Trie *trie);
+		string get_loc();
+		long GetOffsetbyID(unsigned _id);
+		long GetLengthbyID(unsigned _id);
+		char *Mmap;
+		long mmapLength;
 	private:
 		StringIndexFileType type;
 		std::string loc;
 		unsigned num;
 		long empty_offset;
 		FILE *index_file, *value_file;
-
+		//never delete this->trie ,because this trie is the trie KVSTORE owns
+		Trie *trie;
 		class IndexInfo
 		{
 			public:
@@ -34,28 +42,64 @@ class StringIndexFile
 		std::vector<IndexInfo>* index_table;
 
 		unsigned buffer_size;
+
 		char *buffer;
+		char *UncompressBuffer;
 
 		class AccessRequest
 		{
 			public:
 				unsigned id;
-				long offset;
-				unsigned length;
-				std::string *str;
-				AccessRequest(unsigned _id, long _offset, unsigned _length, std::string *_str):
-					id(_id), offset(_offset), length(_length), str(_str){};
+				//long offset;	
+				//unsigned length;
+				//std::string *str;
+				unsigned off_str;
+				/*AccessRequest(unsigned _id, long _offset, unsigned _length, std::string *_str):
+					id(_id), offset(_offset), length(_length), str(_str){};*/
+				//AccessRequest(unsigned _id, std::string *_str):
+					//id(_id), str(_str){};
+				AccessRequest(unsigned _id, unsigned _off_str) :
+					id(_id), off_str(_off_str) {};
+				inline bool operator < (const AccessRequest &x) const
+				{
+					return this->id < x.id;
+				}
+				/*
 				inline bool operator < (const AccessRequest &x) const
 				{
 					return this->offset < x.offset;
-				}
+				}*/
 		};
 		std::vector<AccessRequest> request;
 
 //		std::string dictionary_path;
-
+		string *base;
 	public:
+		void flush_file()
+		{	
+			if (this->value_file != NULL)
+				fclose(this->value_file);
+	
+			this->value_file = fopen((this->loc + "value").c_str(), "rb+");
+
+			struct stat statbuf;
+			int fd;
+			stat((this->get_loc() + "value").c_str(), &statbuf);
+			//fd = open((this->get_loc() + "value").c_str(), O_RDONLY);
+			this->mmapLength = (statbuf.st_size / 4096 + 1) * 4096;
+			this->Mmap = (char*)mmap(NULL, this->mmapLength, PROT_READ, MAP_POPULATE | MAP_SHARED, this->value_file, 0);
+			//close(fd);
+
+		}
+		void set_string_base(string *_base)
+		{
+			this->base = _base;
+		}
 		//Trie *trie;
+		void request_reserve(int capcity)
+		{
+			request.reserve(capcity);
+		}
 
 		StringIndexFile(StringIndexFileType _type, std::string _dir, unsigned _num):type(_type), num(_num), empty_offset(0), index_file(NULL), value_file(NULL), buffer_size(0), buffer(NULL)
 		{
@@ -66,7 +110,9 @@ class StringIndexFile
 			if (this->type == Predicate)
 				this->loc = _dir + "/predicate_";
 			this->index_table = new std::vector<IndexInfo>;
-
+			Mmap = NULL;
+			mmapLength = 0;
+			UncompressBuffer = new char[MAX_BLOCK_SIZE*4];
 			//dictionary_path = _dir + "/../dictionary.dc";
 			//trie = new Trie;
 		}
@@ -78,7 +124,10 @@ class StringIndexFile
 				fclose(this->value_file);
 			if (this->buffer != NULL)
 				delete[] this->buffer;
-			delete this->index_table;
+			delete this->index_table;	 
+			if (mmapLength != 0)
+				munmap(Mmap, mmapLength);
+			delete[] UncompressBuffer;
 //			if (this->trie != NULL)
 //				delete trie;
 		}
@@ -94,7 +143,7 @@ class StringIndexFile
 		void setNum(unsigned _num);
 		void save(KVstore &kv_store);
 		void load();
-
+		static void *thread_read(void * argv);
 		inline void allocBuffer(unsigned length)
 		{
 			if (this->buffer_size <= length)
@@ -112,11 +161,13 @@ class StringIndexFile
 		}
 
 		bool randomAccess(unsigned id, std::string *str, bool real = true);
-		inline void addRequest(unsigned id, std::string *str)
+
+		inline void addRequest(unsigned id, unsigned off_base)
 		{
-			this->request.push_back(AccessRequest(id, (*this->index_table)[id].offset, (*this->index_table)[id].length, str));
+			this->request.push_back(AccessRequest(id, off_base));
 		}
-		void trySequenceAccess(bool real = true);
+
+		void trySequenceAccess(bool real = true, pthread_t tidp = -1);
 
 		void change(unsigned id, KVstore &kv_store);
 		void disable(unsigned id);
@@ -133,7 +184,8 @@ class StringIndex
 
 	public:
 //		Trie *trie;
-
+		void SetTrie(Trie* trie);
+		vector<StringIndexFile*> get_three_StringIndexFile();
 		StringIndex(std::string _dir, unsigned _entity_num = 0, unsigned _literal_num = 0, unsigned _predicate_num = 0):
 			entity(StringIndexFile::Entity, _dir, _entity_num), literal(StringIndexFile::Literal, _dir, _literal_num), predicate(StringIndexFile::Predicate, _dir, _predicate_num)
 		{
@@ -174,9 +226,8 @@ class StringIndex
 		void load();
 
 		bool randomAccess(unsigned id, std::string *str, bool is_entity_or_literal = true, bool real = true);
-		void addRequest(unsigned id, std::string *str, bool is_entity_or_literal = true);
-		void trySequenceAccess(bool real = true);
-
+		void addRequest(unsigned id, unsigned off_base, bool is_entity_or_literal = true);
+		void trySequenceAccess(bool real = true, pthread_t tidp = -1);
 		void change(std::vector<unsigned> &ids, KVstore &kv_store, bool is_entity_or_literal = true);
 		void disable(std::vector<unsigned> &ids, bool is_entity_or_literal = true);
 };
