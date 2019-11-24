4bf0514222ec2342f48f22d838eea9b737e5b41c
LiWenJieHnu <liwenjie@hnu.edu.cn>
2019-11-24 11:35:08 +0800

    Revert "restore the RDFParser.cpp"
    This reverts commit 6da667db466e6142b20786256d02cccc91e7350d.

--- Parser/RDFParser.h
+++ Parser/RDFParser.h
@@ -5,7 +5,6 @@
 #include "../Util/Triple.h"
 #include "TurtleParser.h"
 
-
 using namespace std;
 
 class RDFParser
@@ -26,7 +25,5 @@ public:
     string parseFile(TripleWithObjType* _triple_array, int& _triple_num);
     string parseFileSample(TripleWithObjType* _triple_array, int& _triple_num, int UPBOUND);
    string parseString(string _str, TripleWithObjType* _triple_array, int& _triple_num);
-
-   /*bool isNumber(string _str);*/
 };
 #endif
