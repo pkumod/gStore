09df67781ba41b6db587d783774545e97e4e528f
VEI7 <jiaqi97@pku.edu.cn>
2021-01-15 11:46:35 +0800

    solve problems in auto-run module

--- scripts/add-service.sh
+++ scripts/add-service.sh
@@ -3,7 +3,9 @@ echo -e "\033[43;35m run this script in bash using root priviledge \033[0m \n"
 
 BASEDIR=$(pwd)
 DIR=$BASEDIR/autorun-ghttp.sh
-echo "cd $BASEDIR/..">$DIR
+echo "export CXX=/usr/local/bin/gcc">$DIR
+echo "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64:\$LD_LIBRARY_PATH">>$DIR
+echo "cd $BASEDIR/..">>$DIR
 echo "bin/ghttp $1 &">>$DIR
 echo $DIR>>/etc/rc.local
 chmod +x $DIR
