bd167d26e9f8401d4a6b8b0790c83552deb7ed66
liwenjie <liwenjiehn@pku.edu.cn>
2021-08-03 11:23:03 +0800

    Revert "delete the workflow source"
    This reverts commit feb2ae8ea110e30be297eced8c175c19c86127e7.

---
+++ workflow/WebUrl.cc
@@ -0,0 +1,25 @@
+#include "WebUrl.h"
+
+string WebUrl::Request(const string& request) const {
+	smatch result;
+	 if (regex_search(_url.cbegin(), _url.cend(), result, regex(request + "=(.*?)&"))) {
+	             // 匹配具有多个参数的url
+			
+			             // *? 重复任意次，但尽可能少重复  
+		             return result[1];
+	
+	}
+	else if (regex_search(_url.cbegin(), _url.cend(), result, regex(request + "=(.*)"))) {
+	            // 匹配只有一个参数的url
+		
+		            return result[1];
+	
+	}
+	else {
+		           // 不含参数或制定参数不存在
+			
+			             return string();
+		
+	}
+
+}
