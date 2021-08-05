3067199260cf0d6745cea8c04deece6131d96fa2
liwenjie <liwenjiehn@pku.edu.cn>
2021-08-05 19:14:12 +0800

    alter the file

--- Util/WebUrl.cpp
+++ Util/WebUrl.cpp
@@ -1,29 +1,6 @@
 #include "WebUrl.h"
 
 using namespace std;
-string WebUrl::Request(string& url,string& request) {
-	smatch result;
-	if (regex_search(url.cbegin(), url.cend(), result, regex(request + "=(.*?)&"))) {
-		// 匹配具有多个参数的url
-
-				// *? 重复任意次，但尽可能少重复  
-		return result[1];
-
-	}
-	else if (regex_search(url.cbegin(), url.cend(), result, regex(request + "=(.*)"))) {
-		// 匹配只有一个参数的url
-
-		return result[1];
-
-	}
-	else {
-		// 不含参数或制定参数不存在
-
-		return string();
-
-	}
-}
-
 string WebUrl::CutParam(string url, string param)
 {
 	int index = url.find("?" + param + "=");
