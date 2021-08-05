8fb6a5bc92c726ccca504d72460a628ab142c47a
liwenjie <liwenjiehn@pku.edu.cn>
2021-08-05 19:14:12 +0800

    alter the file

--- Util/WebUrl.h
+++ Util/WebUrl.h
@@ -2,17 +2,13 @@
 #ifndef WEB_URL_H_
 #define WEB_URL_H)
 
-#include <regex>
+
 #include <string>
 using namespace std;
 class WebUrl {
 public:
-	WebUrl(const string& url): _url(url){}
-	WebUrl(string&& url) :_url(move(url)) {}
-
-	static string Request(string& url,string& request);
+	
 	static string CutParam(string url, string param);
-private:
-	string _url;
+
 };
 #endif //WEB_URL_H_
