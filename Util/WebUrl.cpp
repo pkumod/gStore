aaab6cd694db01d20de8545b0183872681ad5b60
liwenjie <liwenjiehn@pku.edu.cn>
2021-08-05 16:22:51 +0800

    alter the WebUrl

--- Util/WebUrl.cpp
+++ Util/WebUrl.cpp
@@ -1,16 +1,16 @@
 #include "WebUrl.h"
 
 using namespace std;
-WebUrl::Request(const string& request) const {
+WebUrl::Request(string& url,string& request) const {
 	smatch result;
-	if (regex_search(_url.cbegin(), _url.cend(), result, regex(request + "=(.*?)&"))) {
+	if (regex_search(url.cbegin(), url.cend(), result, regex(request + "=(.*?)&"))) {
 		// 匹配具有多个参数的url
 
 				// *? 重复任意次，但尽可能少重复  
 		return result[1];
 
 	}
-	else if (regex_search(_url.cbegin(), _url.cend(), result, regex(request + "=(.*)"))) {
+	else if (regex_search(url.cbegin(), url.cend(), result, regex(request + "=(.*)"))) {
 		// 匹配只有一个参数的url
 
 		return result[1];
