4236401d3062598525c5f260e8d5dbb8b7dfd304
liwenjie <liwenjiehn@pku.edu.cn>
2021-08-10 15:17:56 +0800

    alter the apiserver

--- Main/gapiserver.cpp
+++ Main/gapiserver.cpp
@@ -417,7 +417,7 @@ void process(WFHttpTask* server_task)
 	cout << "method:"<<req->get_method() << endl;
 
 
-	char* method_char = req->get_method();
+	const char* method_char = req->get_method();
 	string method(method_char);
 	if (method == "GET")
 	{
