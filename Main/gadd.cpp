a55cabfd1793207f1bda03827f786619b6912e1b
liwenjie <liwenjiehn@pku.edu.cn>
2021-08-14 19:06:31 +0800

    alter the gadd

--- Main/gadd.cpp
+++ Main/gadd.cpp
@@ -115,37 +115,20 @@ Log.init("slog.properties");
 		Log.Info("finish loading.");
 		//_db.insert(argv[2]);
 		//_db.remove(argv[2]);
-		_db.insert(filename, false, nullptr);
+		long tv_begin = Util::get_cur_time();
+		_db->insert(filename, false, nullptr);
+		long tv_end = Util::get_cur_time();
 		//cout << "finish insert data" << endl;
 		
-		Log.Info("please input the complete command:");
+		Log.Info("after insert,used "<< (tv_end - tv_begin) << " ms");
+		delete _db;
+		long tv_end1 = Util::get_cur_time();
+		Log.Info("persistence on disk" << (tv_end1 - tv_end) << " ms");
 
 
-	Database *_db = new Database(db_folder);
-	_db->load();
-	cout << "finish loading" << endl;
-	//_db.insert(argv[2]);
-	//_db.remove(argv[2]);
-	long tv_begin = Util::get_cur_time();
-	_db->insert(argv[2], false, nullptr);
-	long tv_end = Util::get_cur_time();
-	cout << "after insert, used " << (tv_end - tv_begin) << " ms" << endl;
-	//string query = string(argv[2]);
-	//query = Util::getQueryFromFile(query.c_str());
-	//if (query.empty())
-	//{
-	//return 0;
-	//}
-	//printf("query is:\n%s\n\n", query.c_str());
 
-	//ResultSet _rs;
-	//_db.query(query, _rs, stdout);
 
-	//TODO:to test insert, delete and modify
-	//read from file or just several triples written here
-	delete _db;
-	long tv_end1 = Util::get_cur_time();
-	cout << "persistence on disk" << (tv_end1 - tv_end) << " ms" << endl
+	
 	}
 	
 
