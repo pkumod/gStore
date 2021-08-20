8ba1d731730e2eeae2e28e25960af4a5c8285771
liwenjie <liwenjiehn@pku.edu.cn>
2021-08-20 17:03:03 +0800

    alter the ghttp

--- Util/Util.cpp
+++ Util/Util.cpp
@@ -281,7 +281,10 @@ bool Util::configure_new()
     Util::setGlobalConfig(ini_parser, "ghttp", "db_path");
     Util::setGlobalConfig(ini_parser, "ghttp", "backup_path");
     Util::setGlobalConfig(ini_parser, "ghttp", "ip");
+    Util::setGlobalConfig(ini_parser, "ghttp", "ip_allow_path");
+    Util::setGlobalConfig(ini_parser, "ghttp", "ip_deny_path");
     Util::setGlobalConfig(ini_parser, "system", "version");
+  
     cout << "the current settings are as below: " << endl;
     cout << "key : value" << endl;
     cout << "------------------------------------------------------------" << endl;
