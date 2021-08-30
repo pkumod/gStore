2f1471c52322aaa3b0ea2597c7fc409287c0abe8
雨奇 周 <375840024@qq.com>
2021-08-30 11:12:58 +0800

    extract class TopKSearchPlan from TopKUtil

--- Query/topk/TopKUtil.h
+++ Query/topk/TopKUtil.h
@@ -14,70 +14,14 @@
 #include "../../Query/QueryTree.h"
 #include "../../Query/IDList.h"
 #include "../../Database/TableOperator.h"
+#include "TopKSearchPlan.h"
 
 #define TOPK_DEBUG_INFO
 
 namespace TopKUtil{
-enum class EdgeDirection{IN,OUT,NoEdge};
-
-// something like FeedTwoNodes, but this one is only used in
-// Top-K searching
-struct TreeEdge{
-    std::vector<bool> predicate_constant_;
-    std::vector<TYPE_ENTITY_LITERAL_ID> predicate_ids_;
-    std::vector<TopKUtil::EdgeDirection> directions_;
-    void ChangeOrder();
-};
-
-}
-
-
-// #define SHOW_SCORE
-// only Vars
-struct TopKTreeNode{
-  int var_id;
-  std::vector<std::shared_ptr<TopKUtil::TreeEdge>> tree_edges_;
-  std::vector<TopKTreeNode*> descendents_;
-  size_t descendents_fr_num_;
-  size_t descendents_ow_num_;
-};
-
-/**
- * The General Top-k Search Plan Structure:
- * 1. filtering all entity vars
- * 2. build top-k iterators
- * 3. get top-k results
- * 3.1 check non-tree edges
- * 3.2 fill selected predicate vars
- */
-class TopKTreeSearchPlan {
- private:
-  std::size_t total_vars_num_;
-  static std::size_t CountDepth(map<TYPE_ENTITY_LITERAL_ID,vector<TYPE_ENTITY_LITERAL_ID>> &neighbours, TYPE_ENTITY_LITERAL_ID root_id, std::size_t total_vars_num);
-  void AdjustOrder();
- public:
-  explicit TopKTreeSearchPlan(shared_ptr<BGPQuery> bgp_query, KVstore *kv_store, Statistics *statistics, QueryTree::Order expression,
-                              shared_ptr<map<TYPE_ENTITY_LITERAL_ID,shared_ptr<IDList>>> id_caches);
-  // The first tree to search
-  TopKTreeNode* tree_root_;
-  // Recursive delete
-  ~TopKTreeSearchPlan();
-  // The Edges that left behind
-  // It can be used in two ways:
-  // 1 . when filtering, use non tree edges to early filter
-  // 2 . when enumerating , use non tree edges to make sure correctness
-  std::vector<StepOperation> non_tree_edges;
-  // The predicate been selected,
-  // We process these vars when all the entity vars have been filled
-  std::vector<StepOperation> selected_predicate_edges;
-
-  std::string DebugInfo();
-};
-
-namespace TopKUtil{
 
 void CalculatePosVarMappingNode(TopKTreeNode* top_k_tree_node,shared_ptr<std::map<TYPE_ENTITY_LITERAL_ID, TYPE_ENTITY_LITERAL_ID>> pos_var_mapping);
-shared_ptr<std::map<TYPE_ENTITY_LITERAL_ID, TYPE_ENTITY_LITERAL_ID>> CalculatePosVarMapping(shared_ptr<TopKTreeSearchPlan> search_plan);
+shared_ptr<std::map<TYPE_ENTITY_LITERAL_ID, TYPE_ENTITY_LITERAL_ID>> CalculatePosVarMapping(shared_ptr<TopKSearchPlan> search_plan);
 
 double GetScore(string &v, stringstream &ss);
 void GetVarCoefficientsTreeNode(QueryTree::CompTreeNode *comp_tree_node,
@@ -125,7 +69,7 @@ std::map<TYPE_ENTITY_LITERAL_ID,std::shared_ptr<FRIterator>>  GenerateFRs(int pa
                                                             std::set<TYPE_ENTITY_LITERAL_ID>& deleted_parents,
                                                             TopKTreeNode *child_tree_node, Env *env);
 
-FRIterator* BuildIteratorTree(const shared_ptr<TopKTreeSearchPlan> tree_search_plan,Env *env);
+FRIterator* BuildIteratorTree(const shared_ptr<TopKSearchPlan> tree_search_plan, Env *env);
 
 void UpdateIDList(shared_ptr<IDList> valid_id_list, unsigned* id_list, unsigned id_list_len,bool id_list_prepared);
 
