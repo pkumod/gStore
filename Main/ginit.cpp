ecd80b3de421f8928dd8f46ff3b6ec9d423ca065
root <liwenjiehn@pku.edu.cn>
2021-09-19 15:31:32 +0800

    fix the lock bug during loading

--- Main/ginit.cpp
+++ Main/ginit.cpp
@@ -29,7 +29,7 @@ int main(int argc, char * argv[])
 		cout << "\t bin/gadd -h" << endl;*/
 		long tv_begin = Util::get_cur_time();
 		cout<<"begin rebuild the system database ...."<<endl;
-		if (boost::filesystem::exists("system.db"))
+		if (Util::dir_exist("system.db"))
 		{
 			string cmd;
 			cmd = "rm -r system.db";
@@ -100,15 +100,74 @@ int main(int argc, char * argv[])
 			cout << "Options:" << endl;
 			cout << "\t-h,--help\t\tDisplay this message." << endl;
 			cout << "\t-db,--database,\t\t The database names.Use , to split database name. e.g. databaseA,databaseB" << endl;
-			cout << "If you want to rebuild the system database,please use bin/ginit \n" << endl;
-			cout << "If you want to add database info into system database,please use bin/ginit -db db1,db2,...\n" << endl;
+			cout << "If you want to rebuild the system database,please use bin/ginit \n"
+				 << endl;
+			cout << "If you want to add database info into system database,please use bin/ginit -db db1,db2,...\n"
+				 << endl;
 			cout << endl;
 			return 0;
+		}
+		else if (command == "-m" || command == "--make")
+		{
+			if (Util::dir_exist("system.db") == false)
+			{
+				cout << "system.db is not exist. Now create it." << endl;
+				long tv_begin = Util::get_cur_time();
+
+				string _db_path = "system";
+				string _rdf = "./data/system/system.nt";
+				Database *_db = new Database(_db_path);
+				bool flag = _db->build(_rdf);
+				if (flag)
+				{
+
+					ofstream f;
+					f.open("./" + _db_path + ".db/success.txt");
+					f.close();
+
+					cout << "system.db built successfully!" << endl;
+					delete _db;
+					_db = NULL;
+					Util::init_backuplog();
+					Util::configure_new();
+					string version = Util::getConfigureValue("version");
+					string update_sparql = "insert data {<CoreVersion> <value> " + version + ". }";
+
+					ResultSet _rs;
+					FILE *ofp = stdout;
+					string msg;
+					_db = new Database(_db_path);
+					_db->load();
+					int ret = _db->query(update_sparql, _rs, ofp);
+					if (ret >= 0)
+						msg = "update num : " + Util::int2string(ret);
+					else //update error
+						msg = "update failed.";
+					if (ret != -100)
+						cout << "Insert data result:" + msg << endl;
+
+					delete _db;
+					_db = NULL;
+					long tv_end = Util::get_cur_time();
+					//stringstream ss;
+					cout << "system.db init successfully! Used " << (tv_end - tv_begin) << " ms" << endl;
+					//Log.Info(ss.str().c_str());
+					return 0;
+				}
+				else
+				{
+					cout << "Build RDF database failure!" << endl;
+					return 0;
+				}
+			}
+			else{
+				cout<<"the system database is exist, skip the init system database."<<endl;
+			}
 		}
 		else
 		{
 			//cout << "the command is not complete." << endl;
-			cout<<"Invalid arguments! Input \"bin/ginit -h\" for help."<<endl;
+			cout << "Invalid arguments! Input \"bin/ginit -h\" for help." << endl;
 			return 0;
 		}
 	}
