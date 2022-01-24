f728fb98662d619166b1c111ecfb5d7ba89e9e5d
Michelle Pang <michelle.py@pku.edu.cn>
2022-01-24 20:56:21 +0800

    fix(scripts): adapt parser_test.sh to new command and output format

--- scripts/parser_test.sh
+++ scripts/parser_test.sh
@@ -9,6 +9,8 @@ gdrop='bin/gdrop '
 
 all_passed=true
 
+$gdrop -db parser_test > /dev/null
+
 for i in $(seq 1 $query_num)
 do
     data=$data_dir"parser_d"$i".ttl"
@@ -17,11 +19,11 @@ do
 
     "grep" "." $result | "sort" > "result_a.txt"
 
-    $gbuild "parser_test" $data > "tmp.txt"
-    $gquery "parser_test" $query | "grep" "-A" "10000" "final result is :" > "tmp.txt"
+    $gbuild -db parser_test -f $data > tmp.txt
+    $gquery -db parser_test -q $query | grep -A 10000 "final result is :" > tmp.txt
     "sed" "-i" "1d" "tmp.txt"
     "sed" "-i" "\$d" "tmp.txt"
-    # "sed" "-i" "\$d" "tmp.txt"
+    "sed" "-i" "\$d" "tmp.txt"
     "grep" "." "tmp.txt" | "sort" > "result_b.txt"
     
     "diff" "result_a.txt" "result_b.txt" > "equal.txt"
@@ -32,7 +34,7 @@ do
         echo "parser test #"$i" passed"
     fi
 
-	$gdrop "parser_test" > "tmp.txt"
+	$gdrop -db parser_test > tmp.txt
     "rm" "result_a.txt" "result_b.txt" "tmp.txt" "equal.txt" 
 done
 
