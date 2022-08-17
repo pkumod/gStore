5afb0be19dc616cc4d63dcd24bbe2b47968cf760
pangyue <michelle.py@pku.edu.cn>
2022-08-17 22:47:19 +0800

    feat(Query): support k-hop path enumeration

--- Query/PathQueryHandler.cpp
+++ Query/PathQueryHandler.cpp
@@ -1531,7 +1531,159 @@ vector<int> PathQueryHandler::kHopReachablePath(int uid, int vid, bool directed,
 */
 vector<vector<int>> PathQueryHandler::kHopEnumeratePath(int uid, int vid, bool directed, int k, const std::vector<int> &pred_set)
 {
-	return vector<vector<int>>();
+	vector<vector<int>> ret;
+	vector<pair<int, int>> s;
+	vector<int> bar(getVertNum(), 0);
+	// Use reverse kBFS from destination to refine bar
+	queue<int> q;
+	q.push(vid);
+	while (!q.empty())
+	{
+		int cur = q.front();
+		q.pop();
+		if (bar[cur] >= k)
+			continue;
+		for (int pred : pred_set)
+		{
+			int inNum = getInSize(cur, pred);
+			for (int i = 0; i < inNum; ++i)
+			{
+				int to = getInVertID(cur, pred, i);
+				if (bar[to] == 0 && to != vid)
+				{
+					bar[to] = bar[cur] + 1;
+					q.push(to);
+				}
+			}
+			if (directed)
+				continue;
+			int outNum = getOutSize(cur, pred);
+			for (int i = 0; i < outNum; ++i)
+			{
+				int to = getOutVertID(cur, pred, i);
+				if (bar[to] == 0 && to != vid)
+				{
+					bar[to] = bar[cur] + 1;
+					if (to == 2)
+						cout << "bar[to] = " << bar[to] << endl;
+					q.push(to);
+				}
+			}
+		}
+	}
+
+	s.push_back(make_pair(uid, -1));	// (vertex_id, pred_id that precedes it)
+	bc_dfs(uid, vid, directed, k, pred_set, s, bar, ret);
+	cout << "ret.size() = " << ret.size() << endl;
+	return ret;
+}
+
+int PathQueryHandler::bc_dfs(int uid, int vid, bool directed, int k, const std::vector<int> &pred_set, vector<pair<int, int>> &s, vector<int> &bar, vector<vector<int>> &paths)
+{
+	int f = k + 1;
+
+	if (uid == vid)
+	{
+		cout << "stack: ";
+		paths.emplace_back();
+		for (size_t i = 0; i < s.size(); i++)
+		{
+			auto pr = s[i];
+			cout << '(' << pr.first << ',' << pr.second << ") ";
+			if (i != 0)
+				paths.back().emplace_back(pr.second);
+			paths.back().emplace_back(pr.first);
+		}
+		cout << endl;
+		cout << "path: ";
+		for (auto ele : paths.back())
+			cout << ele << ' ';
+		cout << endl;
+		s.pop_back();
+		return 0;
+	}
+	else if ((int)s.size() - 1 < k)
+	{
+		int inNum, outNum, to;
+		for (int pred : pred_set)
+		{
+			outNum = getOutSize(uid, pred);
+			for (int i = 0; i < outNum; ++i)
+			{
+				to = getOutVertID(uid, pred, i);
+				bool stacked = false;
+				for (auto pr : s)
+					if (pr.first == to)
+					{
+						stacked = true;
+						break;
+					}
+				if (stacked)
+					continue;
+
+				if ((int)s.size() + bar[to] <= k)
+				{
+					s.push_back(make_pair(to, pred));
+					int next_f = bc_dfs(to, vid, directed, k, pred_set, s, bar, paths);
+					if (next_f != k + 1 && f < next_f + 1)
+						f = next_f + 1;
+				}
+
+			}
+			if (directed)
+				continue;
+			inNum = getInSize(uid, pred);
+			for (int i = 0; i < inNum; ++i)
+			{
+				to = getInVertID(uid, pred, i);
+				bool stacked = false;
+				for (auto pr : s)
+					if (pr.first == to)
+					{
+						stacked = true;
+						break;
+					}
+				if (stacked)
+					continue;
+
+				if ((int)s.size() + bar[to] <= k)
+				{
+					s.push_back(make_pair(to, pred));
+					int next_f = bc_dfs(to, vid, directed, k, pred_set, s, bar, paths);
+					if (next_f != k + 1 && f < next_f + 1)
+						f = next_f + 1;
+				}
+			}
+		}
+	}
+
+	// The following pruning not suitable for labeled multigraphs
+	// if (f == k + 1)
+	// 	bar[uid] = k - s.size() + 2;
+	// else
+	// 	updateBarrier(uid, directed, pred_set, bar, f);
+
+	s.pop_back();
+	return f;
+}
+
+void PathQueryHandler::updateBarrier(int uid, bool directed, const vector<int> &pred_set, std::vector<int> &bar, int l)
+{
+	if (bar[uid] > l)
+	{
+		bar[uid] = l;
+		for (int pred : pred_set)
+		{
+			int inNum = getInSize(uid, pred);
+			for (int i = 0; i < inNum; ++i)
+				updateBarrier(getInVertID(uid, pred, i), directed, pred_set, bar, l + 1);
+			if (directed)
+				continue;
+			int outNum = getOutSize(uid, pred);
+			for (int i = 0; i < outNum; ++i)
+				updateBarrier(getOutVertID(uid, pred, i), directed, pred_set, bar, l + 1);
+		}
+	}
 }
 
 /**
