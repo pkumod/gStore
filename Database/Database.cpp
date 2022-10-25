e3458ff301846995ba40c61ad3a143ebf695f444
lsvih <lsvih@qq.com>
2022-10-25 14:37:20 +0800

    update(query): remove unwanted output.

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -1916,9 +1916,9 @@ int Database::query(const string _query, ResultSet &_result_set, FILE *_fp, bool
 		if (_result_set.output_limit != -1)
 			ans_num = min(ans_num, (long long)_result_set.output_limit);
 		cout << "There has answer: " << ans_num << endl;
-		cout << "final result is : " << endl;
 		if (_fp == stdout)
 		{
+			cout << "final result is : " << endl;
 			_result_set.prettyPrint();
 		}
 		else
