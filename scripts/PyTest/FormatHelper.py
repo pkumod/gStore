9901045531471c813af7dedf26419cb5f28d8e9a
YuqqiZhou <Zyq1998zyQ>
2022-11-21 17:52:02 +0800

    further split python test file

--- scripts/PyTest/FormatHelper.py
+++ scripts/PyTest/FormatHelper.py
@@ -1,12 +1,3 @@
-import os
-import subprocess
-
-
-def ResultAllSameChecker(correct_results, output_results):
-    correct_results.sort()
-    output_results.sort()
-    return correct_results == output_results
-
 
 def GetResultFromFile(result_path: str):
     correct_results = []
@@ -111,58 +102,3 @@ def GetOutputResult(query_out, query_vars):
         for j in range(len(output_vars)):
             output_results[-1][output2query[j]] = output_results_unmapped[j]
     return output_results, output_vars
-
-
-def TestCase(db_name: str, case_name: str, data_path: str,
-             query_path: str, result_path: str, ResultChecker=ResultAllSameChecker):
-    os.system("bin/gdrop -db " + db_name + " > /dev/null")
-    print(".....Running " + case_name + ".....")
-    # Read correct result from file
-    correct_results, query_vars = GetResultFromFile(result_path)
-
-    # Run query
-    os.system("bin/gbuild -db " + db_name + " -f " + data_path + " > /dev/null")
-    child = subprocess.Popen(["bin/gquery", "-db", db_name, "-q", query_path], stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
-
-    query_out = child.stdout.read().decode()
-    res_start = query_out.find("final result is")
-    if res_start == -1:
-        PrintError(case_name, query_vars, correct_results, query_out, [], [])
-        os.system("bin/gdrop -db " + db_name + " > /dev/null")
-        return False
-    query_out = query_out[query_out.find("final result is"):]
-    query_out = query_out.split('\n')
-
-    output_results, output_vars = GetOutputResult(query_out, query_vars)
-    # Compare correct and output results
-    passed = ResultChecker(correct_results, output_results)
-
-    if passed:
-        print("..... " + case_name + " Correct.....")
-    else:
-        PrintError(case_name, query_vars, correct_results, query_out, output_vars, output_results)
-    os.system("bin/gdrop -db " + db_name + " > /dev/null")
-    return passed
-
-
-def ParseTest():
-    query_num = 47
-    data_dir = "scripts/parser_test/"
-    all_passed = True
-    db_name = "parser_test"
-    for i in range(1, query_num + 1):
-        cast_name = "Test #%d" % i
-        data_path = data_dir + "parser_d" + str(i) + ".ttl"
-        query_path = data_dir + "parser_q" + str(i) + ".sql"
-        result_path = data_dir + "parser_r" + str(i) + ".txt"
-        case_passed = TestCase(db_name, cast_name, data_path, query_path, result_path)
-        all_passed = all_passed and case_passed
-    if all_passed:
-        print("All test case have passed")
-    else:
-        print("Some Error Exist!")
-
-
-if __name__ == '__main__':
-    ParseTest()
---
+++ scripts/PyTest/auto_test.py
@@ -0,0 +1,59 @@
+import os
+import subprocess
+
+import ResultChecker
+import FormatHelper
+
+def TestCase(db_name: str, case_name: str, data_path: str,
+             query_path: str, result_path: str, ResultCheckerFunction= ResultChecker.ResultAllSameChecker):
+    os.system("bin/gdrop -db " + db_name + " > /dev/null")
+    print(".....Running " + case_name + ".....")
+    # Read correct result from file
+    correct_results, query_vars = FormatHelper.GetResultFromFile(result_path)
+
+    # Run query
+    os.system("bin/gbuild -db " + db_name + " -f " + data_path + " > /dev/null")
+    child = subprocess.Popen(["bin/gquery", "-db", db_name, "-q", query_path], stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+
+    query_out = child.stdout.read().decode()
+    res_start = query_out.find("final result is")
+    if res_start == -1:
+        FormatHelper.PrintError(case_name, query_vars, correct_results, query_out, [], [])
+        os.system("bin/gdrop -db " + db_name + " > /dev/null")
+        return False
+    query_out = query_out[query_out.find("final result is"):]
+    query_out = query_out.split('\n')
+
+    output_results, output_vars = FormatHelper.GetOutputResult(query_out, query_vars)
+    # Compare correct and output results
+    passed = ResultCheckerFunction(correct_results, output_results)
+
+    if passed:
+        print("..... " + case_name + " Correct.....")
+    else:
+        FormatHelper.PrintError(case_name, query_vars, correct_results, query_out, output_vars, output_results)
+    os.system("bin/gdrop -db " + db_name + " > /dev/null")
+    return passed
+
+
+def ParseTest():
+    query_num = 47
+    data_dir = "scripts/parser_test/"
+    all_passed = True
+    db_name = "parser_test"
+    for i in range(1, query_num + 1):
+        cast_name = "Test #%d" % i
+        data_path = data_dir + "parser_d" + str(i) + ".ttl"
+        query_path = data_dir + "parser_q" + str(i) + ".sql"
+        result_path = data_dir + "parser_r" + str(i) + ".txt"
+        case_passed = TestCase(db_name, cast_name, data_path, query_path, result_path)
+        all_passed = all_passed and case_passed
+    if all_passed:
+        print("All test case have passed")
+    else:
+        print("Some Error Exist!")
+
+
+if __name__ == '__main__':
+    ParseTest()
