361fcc8dd16ac3d9d7de026575b602b5b3a821de
pangyue <michelle.py@pku.edu.cn>
2022-11-27 17:25:04 +0800

    fix(Query): fix path functions calls with duplicate <uid, vid> pairs
    Conflicts:
    	Query/GeneralEvaluation.cpp

--- Query/GeneralEvaluation.h
+++ Query/GeneralEvaluation.h
@@ -22,6 +22,15 @@
 #include <dlfcn.h>
 #include "PathQueryHandler.h"
 
+struct hashFunction
+{
+  size_t operator()(const pair<int , 
+                    int> &x) const
+  {
+    return x.first ^ x.second;
+  }
+};
+
 class GeneralEvaluation
 {
 	private:
