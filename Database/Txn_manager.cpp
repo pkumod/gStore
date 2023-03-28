509911113f561b585ccde79a9845df153656dd86
wangjian <2606583267@qq.com>
2023-03-28 16:40:47 +0800

    fix(Transaction): fix a bug of Commit

--- Database/Txn_manager.cpp
+++ Database/Txn_manager.cpp
@@ -171,8 +171,8 @@ int Txn_manager::Commit(txn_id_t TID)
 		return -1;
 	}
 	else if (txn->GetState() != TransactionState::RUNNING) {
-		cerr << "transaction not in running state! commit failed" << " " << (int)txn->GetState() << endl;
-		checkpoint_lock.unlock();
+		cerr << "transaction not in running state! commit failed" << " " << (int)txn->GetState() << endl;	
+		// checkpoint_lock.unlock();
 		return 1;
 	}
 	txn_id_t CID = this->ArrangeCommitID();
@@ -182,7 +182,7 @@ int Txn_manager::Commit(txn_id_t TID)
 	else
 	{
 		cout << "error! database has been flushed or removed" << endl;
-		checkpoint_lock.unlock();
+		// checkpoint_lock.unlock();
 		return -1;
 	}
 	//writelog(log_str);
