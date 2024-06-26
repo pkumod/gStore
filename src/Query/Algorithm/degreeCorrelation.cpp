12b5a5563978b0a554dea9c85f808838c97f33fc
zhangpan <2748879943@qq.com>
2024-06-26 16:52:29 +0800

    update degreeCorrelation

--- src/Query/Algorithm/degreeCorrelation.cpp
+++ src/Query/Algorithm/degreeCorrelation.cpp
@@ -5,20 +5,36 @@ using namespace std;
 double PathQueryHandler::degreeCorrelation(int uid, int k, const std::vector<int> &pred_sets)
 {
     double ret = 0,e=getSetEdgeNum(pred_sets);
-    double averageD = 0,s=0;
+    double averageD = 0,s1=0,s2=0;
     queue<int> q,de;
     q.push(uid);
     unordered_map<int,bool> vi;
     unordered_map<int,int> degree;
     vector<pair<float,float>> dpair;
     unordered_set<int> Set;
-    auto getVDegree = [&](int id)
+    auto getVDegree = [&](int id,int K)
     {
         int sum = 0;
         for(auto pred:pred_sets)
         {
-            int inSz = getInSize(id,pred);
-            int outSz = getOutSize(id,pred);
+            int inSz = getInSize(id,pred),inNei;
+            int outSz = getOutSize(id,pred),outNei;
+            if(K==0)
+            {
+                for(int i=0;i<inSz;i++)
+                {
+                    inNei = getInVertID(id,pred,i);
+                    if(Set.find(inNei)!=Set.end())
+                        sum++;
+                }
+                for(int i=0;i<outSz;i++)
+                {
+                    outNei = getOutVertID(id,pred,i);
+                    if(Set.find(outNei)!=Set.end())
+                        sum++;
+                }
+            }
+            else
             sum+=inSz+outSz;
         }
         degree[id] = sum;
@@ -36,12 +52,9 @@ double PathQueryHandler::degreeCorrelation(int uid, int k, const std::vector<int
             {
                 inNei = getInVertID(id,pred,i);
                 oneHop_ret.insert(inNei);
-                if(degree.find(inNei)==degree.end())
-                getVDegree(inNei);
                 if(vi[inNei]==false&&(K!=0||(Set.find(inNei)!=Set.end())))
                 {
-                    dpair.emplace_back(pair<double,double>(degree[id],degree[inNei]));
-                    averageD+=0.5*(degree[id]+degree[inNei]);
+                    dpair.emplace_back(pair<double,double>(inNei,id));
                 }
 
             }
@@ -49,29 +62,27 @@ double PathQueryHandler::degreeCorrelation(int uid, int k, const std::vector<int
             {
                 outNei = getOutVertID(id,pred,i);
                 oneHop_ret.insert(outNei);
-                if(degree.find(outNei)==degree.end())
-                getVDegree(outNei);
                 if(vi[outNei]==false&&(K!=0||(Set.find(outNei)!=Set.end())))
                 {
-                    dpair.emplace_back(pair<double,double>(degree[id],degree[outNei]));
-                    averageD+=0.5*(degree[id]+degree[outNei]);
+                    dpair.emplace_back(pair<double,double>(id,outNei));
                 }
             }
         }
         return oneHop_ret;
     };
     Set.insert(uid);  
-    getVDegree(uid);
     while(q.size())
     {
         int vid = q.front();
+        getVDegree(vid,k);
         unordered_set<int> oneHopAdj=oneHop(vid,k);
         vi[vid]=true;
         q.pop();
+        averageD+=degree[vid];
         if(k!=0)
         for(auto v:oneHopAdj)
         {
-            if(vi[v]==false)
+            if(Set.find(v)==Set.end())
             {              
                 q.push(v);
                 Set.insert(v);
@@ -79,12 +90,16 @@ double PathQueryHandler::degreeCorrelation(int uid, int k, const std::vector<int
         }
         if(k>0) k--;
     }
-    averageD/=dpair.size();
+    averageD/=Set.size();
     for(auto v:dpair)
-    {
-        s+=pow((v.first+v.second)/2-averageD,2);
-        ret+=(v.first-averageD)*(v.second-averageD);
+    {        
+        s1+=pow(degree[v.first]-averageD,2);s2+=pow(degree[v.second]-averageD,2);
+        ret+=(degree[v.first]-averageD)*(degree[v.second]-averageD);
     }
-    ret /= s;
+    s1 = pow(s1,0.5);s2=pow(s2,0.5);
+    if(s1*s2==0)
+        ret=0;
+    else
+        ret /= (s1*s2);
     return ret;
 }
