2749cafe1c33440d3ba5202324156f2609a04bf4
hexuejiang <1632802996@qq.com>
2024-07-24 17:14:11 +0800

    update conf:set query min memory

--- Util/Util.cpp
+++ Util/Util.cpp
@@ -135,6 +135,7 @@ Util::configure()
     Util::setGlobalConfig(ini_parser, "system", "root_username", "root");
     Util::setGlobalConfig(ini_parser, "system", "root_password", "123456");
     Util::setGlobalConfig(ini_parser, "system", "system_username", "system");
+    Util::setGlobalConfig(ini_parser, "system", "min_memory", "1");
     // Util::setGlobalConfig(ini_parser, "system", "pfn_base_path", "./pfn/");
     Util::setGlobalConfig(ini_parser, "system", "licensetype", "opensource");
     Util::system_path = Util::getConfigureValue("system_path");
