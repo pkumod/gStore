3400c9a37b92980644cd0ee54e6438a7c9fc77cf
hexuejiang <1632802996@qq.com>
2024-08-13 14:16:08 +0800

    update version_checkout.sh

--- scripts/version_checkout.sh
+++ scripts/version_checkout.sh
@@ -59,7 +59,7 @@ backup_path="version$new_version"'_'$current_time'/'
 mkdir -p $backup_path
 conf_path=""
 conf_ini="conf.ini"
-ini_conf="ini.conf"
+ini_conf="init.conf"
 ipAllow="ipAllow.config"
 ipDeny="ipDeny.config"
 slog_properties="slog.properties"
@@ -81,6 +81,10 @@ cp -r $pfn $backup_path
 temp_show_log=$new_path'/show'$current_time.log
 # echo "temp_show_log: $temp_show_log"
 bin/gshow > $temp_show_log
+if [ $? -ne 0 ]; then
+    echo "bin/gshow execution failure !!!"
+    exit
+fi
 temp_database_log=$new_path'/tmp'$current_time.log
 tac "$temp_show_log" | grep '\-\-\-\-\-\-\-\-\-\-\-\-' -m 1 -A 10000 > $backup_path$temp_database_log
 
@@ -93,7 +97,7 @@ if [[ $git_version < "git version 2.23" ]]; then
     git checkout -- api/http/cpp/example/POST-example
     git checkout -- api/http/cpp/example/Transaction-example
     git checkout -- $conf_path'conf.ini'
-    git checkout -- ini.conf
+    git checkout -- init.conf
     git checkout -- $conf_path'slog.properties'
     git checkout -- $conf_path'slog.stdout.properties'
 else
@@ -103,13 +107,21 @@ else
     git restore api/http/cpp/example/POST-example
     git restore api/http/cpp/example/Transaction-example
     git restore $conf_path'conf.ini'
-    git restore ini.conf
+    git restore init.conf
     git restore $conf_path'slog.properties'
     git restore $conf_path'slog.stdout.properties'
 fi
 
 git checkout -b $new_version origin/$new_version
+if [ $? -ne 0 ]; then
+    echo "checkout origin $new_version failure !!!"
+    exit
+fi
 git checkout $new_version
+if [ $? -ne 0 ]; then
+    echo "checkout $new_version failure !!!"
+    exit
+fi
 if [[ $new_version < "1.3" ]]; then
     make clean
     make pre
@@ -174,12 +186,8 @@ if [[ -n $old_db_home ]]; then
     # echo $is_enable
     if [[ $is_enable != '#' ]]; then
         pos=$(echo "${old_db_home}" | awk -F '=' '{print $1}' | wc -c)
-        for i in ${old_db_home:${pos}:${#old_db_home}-${pos}};
-        do
-            if [[ $i != ' ' ]]; then
-                old_db_home_path=$old_db_home_path$i
-            fi
-        done
+        temp_d=${old_db_home:${pos}:${#old_db_home}-${pos}}
+        old_db_home_path=$(echo ${temp_d//\"/})
     fi
 fi
 
@@ -205,15 +213,15 @@ fi
 for database_name in ${database_list[@]}
 do
     # 新版本已经有对应库是否覆盖旧版本
-    old_path=$old_db_home_path$database_name'.db'
-    new_path=$new_db_home_path$database_name'.db'
+    temp_old_path=$old_db_home_path'/'$database_name'.db'
+    temp_new_path=$new_db_home_path'/'$database_name'.db'
     # echo $old_path $new_path
-    if [ -d $new_path ]; then
+    if [ -d $temp_new_path ]; then
         read -r -p "the new version already has a $database_name.db, overwrite [Y/n] " input
         case $input in
             [yY][eE][sS]|[yY])
-                rm -rf $new_path
-                cp -rf $old_path $new_db_home_path
+                rm -rf $temp_new_path
+                cp -rf $temp_old_path $new_db_home_path
                 echo "overwrite succeffully"
                 ;;
             [nN][oO]|[nN])
@@ -224,11 +232,31 @@ do
                 ;;
 	    esac
     else
-        cp -rf $old_path $new_db_home_path
+        cp -rf $temp_old_path $new_db_home_path
     fi
 done
 
-# ------------新版本初始化所有数据库-------------------
+# ------------新版本数据迁移-------------------
+if [[ $new_version < "1.3" ]]; then
+    if [[ $old_db_home_path ]] && [[ $new_db_home_path ]] && [[ $old_db_home_path != $new_db_home_path ]]; then
+        temp_new_path='# db_home=\".\"'
+        replace_path=$(grep "db_home" conf.ini)
+        if [[ -z $replace_path ]]; then
+            replace_path=$(grep "db_home" ini.conf)
+        fi
+        replace_path=$(echo ${replace_path//\"/\\\"})
+        replace_path=$(echo ${replace_path//\//\\\/})
+        path='s/'$temp_new_path'/'$replace_path'/g'
+        sed -i $path conf.ini
+    fi
+fi
+
+if [[ $cur_version < "1.3" ]] && [[ $new_version > "1.2" ]]; then
+    mv ./pfn/cpp ./pfn/cpp.bak
+    cp -rf $backup_path'fun/' ./pfn/
+    mv ./pfn/fun ./pfn/cpp/
+fi
+
 for database_name in ${database_list[@]}
 do
     if [[ $database_name != 'system' ]]; then
