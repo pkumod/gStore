c09e3a27ee5612d5fb6637a4b6eb12dc461400b3
hexuejiang <1632802996@qq.com>
2024-08-13 14:13:20 +0800

    update version_update.sh, version_checkout.sh

--- scripts/version_checkout.sh
+++ scripts/version_checkout.sh
@@ -32,6 +32,11 @@ if [[ $cur_version < "1.0" ]]; then
     exit
 fi
 
+if [[ $new_version < $cur_version ]]; then
+    echo "the new version shouldn't lower current version !!!"
+    exit
+fi
+
 echo "current version is $cur_version"
 
 # 相同版本重新编译即可
@@ -52,34 +57,71 @@ gstore_path=$(pwd)'/'
 current_time=$(date "+%Y%m%d%H%M%S")
 backup_path="version$new_version"'_'$current_time'/'
 mkdir -p $backup_path
+conf_path=""
 conf_ini="conf.ini"
-ini_conf="ini.conf"
+ini_conf="init.conf"
 ipAllow="ipAllow.config"
 ipDeny="ipDeny.config"
+slog_properties="slog.properties"
+slog_stdout_properties="slog.stdout.properties"
 pfn="fun"
-if [[ $cur_version < "1.3" ]]; then
-    cp -f $conf_ini $backup_path
-    cp -f $ini_conf $backup_path
-    cp -f $ipAllow $backup_path
-    cp -f $ipDeny $backup_path
-else
+if [[ $cur_version > "1.2" ]]; then
+    conf_path="conf/"
     pfn="pfn"
-    cp -f 'conf/'$conf_ini $backup_path
-    cp -f 'conf/'$ipAllow $backup_path
-    cp -f 'conf/'$ipDeny $backup_path
-    cp -r $pfn $backup_path
 fi
+cp -f $conf_path$conf_ini $backup_path
+cp -f $ini_conf $backup_path
+cp -f $conf_path$ipAllow $backup_path
+cp -f $conf_path$ipDeny $backup_path
+cp -f $conf_path$slog_properties $backup_path
+cp -f $conf_path$slog_stdout_properties $backup_path
+cp -r $pfn $backup_path
 
 # gshow查看数据库
 temp_show_log=$new_path'/show'$current_time.log
 # echo "temp_show_log: $temp_show_log"
 bin/gshow > $temp_show_log
+if [ $? -ne 0 ]; then
+    echo "bin/gshow execution failure !!!"
+    exit
+fi
 temp_database_log=$new_path'/tmp'$current_time.log
 tac "$temp_show_log" | grep '\-\-\-\-\-\-\-\-\-\-\-\-' -m 1 -A 10000 > $backup_path$temp_database_log
 
-# 切换版本
+# 切换版本,将远程仓库的文件回退忽略掉
+git_version=$(git version)
+if [[ $git_version < "git version 2.23" ]]; then
+    git checkout -- $conf_path'backup.json'
+    git checkout -- .gitignore
+    git checkout -- api/http/cpp/example/GET-example
+    git checkout -- api/http/cpp/example/POST-example
+    git checkout -- api/http/cpp/example/Transaction-example
+    git checkout -- $conf_path'conf.ini'
+    git checkout -- init.conf
+    git checkout -- $conf_path'slog.properties'
+    git checkout -- $conf_path'slog.stdout.properties'
+else
+    git restore $conf_path'backup.json'
+    git restore .gitignore
+    git restore api/http/cpp/example/GET-example
+    git restore api/http/cpp/example/POST-example
+    git restore api/http/cpp/example/Transaction-example
+    git restore $conf_path'conf.ini'
+    git restore init.conf
+    git restore $conf_path'slog.properties'
+    git restore $conf_path'slog.stdout.properties'
+fi
+
 git checkout -b $new_version origin/$new_version
+if [ $? -ne 0 ]; then
+    echo "checkout origin $new_version failure !!!"
+    exit
+fi
 git checkout $new_version
+if [ $? -ne 0 ]; then
+    echo "checkout $new_version failure !!!"
+    exit
+fi
 if [[ $new_version < "1.3" ]]; then
     make clean
     make pre
@@ -144,12 +186,8 @@ if [[ -n $old_db_home ]]; then
     # echo $is_enable
     if [[ $is_enable != '#' ]]; then
         pos=$(echo "${old_db_home}" | awk -F '=' '{print $1}' | wc -c)
-        for i in ${old_db_home:${pos}:${#old_db_home}-${pos}};
-        do
-            if [[ $i != ' ' ]]; then
-                old_db_home_path=$old_db_home_path$i
-            fi
-        done
+        temp_d=${old_db_home:${pos}:${#old_db_home}-${pos}}
+        old_db_home_path=$(echo ${temp_d//\"/})
     fi
 fi
 
@@ -165,12 +203,6 @@ if [[ $old_db_home_path = '.' ]]; then
     old_db_home_path=""
 fi
 
-if [[ $cur_version < "1.3" ]]; then
-    old_db_home_path='.'
-else
-    old_db_home_path='./dbhome/'
-fi
-
 new_db_home_path=''
 if [[ $new_version < "1.3" ]]; then
     new_db_home_path=$old_db_home_path
@@ -181,15 +213,15 @@ fi
 for database_name in ${database_list[@]}
 do
     # 新版本已经有对应库是否覆盖旧版本
-    old_path=$old_db_home_path$database_name'.db'
-    new_path=$new_db_home_path$database_name'.db'
+    temp_old_path=$old_db_home_path'/'$database_name'.db'
+    temp_new_path=$new_db_home_path'/'$database_name'.db'
     # echo $old_path $new_path
-    if [ -d $new_path ]; then
+    if [ -d $temp_new_path ]; then
         read -r -p "the new version already has a $database_name.db, overwrite [Y/n] " input
         case $input in
             [yY][eE][sS]|[yY])
-                rm -rf $new_path
-                cp -rf $old_db_home_path$database_name'.db' $new_db_home_path
+                rm -rf $temp_new_path
+                cp -rf $temp_old_path $new_db_home_path
                 echo "overwrite succeffully"
                 ;;
             [nN][oO]|[nN])
@@ -199,13 +231,37 @@ do
                 echo "Invalid input..."
                 ;;
 	    esac
+    else
+        cp -rf $temp_old_path $new_db_home_path
     fi
 done
 
-# ------------新版本初始化所有数据库-------------------
+# ------------新版本数据迁移-------------------
+if [[ $new_version < "1.3" ]]; then
+    if [[ $old_db_home_path ]] && [[ $new_db_home_path ]] && [[ $old_db_home_path != $new_db_home_path ]]; then
+        temp_new_path='# db_home=\".\"'
+        replace_path=$(grep "db_home" conf.ini)
+        if [[ -z $replace_path ]]; then
+            replace_path=$(grep "db_home" ini.conf)
+        fi
+        replace_path=$(echo ${replace_path//\"/\\\"})
+        replace_path=$(echo ${replace_path//\//\\\/})
+        path='s/'$temp_new_path'/'$replace_path'/g'
+        sed -i $path conf.ini
+    fi
+fi
+
+if [[ $cur_version < "1.3" ]] && [[ $new_version > "1.2" ]]; then
+    mv ./pfn/cpp ./pfn/cpp.bak
+    cp -rf $backup_path'fun/' ./pfn/
+    mv ./pfn/fun ./pfn/cpp/
+fi
+
 for database_name in ${database_list[@]}
 do
     if [[ $database_name != 'system' ]]; then
         bin/ginit -db $database_name
     fi
 done
+
+echo "version update successfully, backup file in the $backup_path"
--- scripts/version_update.sh
+++ scripts/version_update.sh
@@ -2,15 +2,6 @@
 
 # bash scripts/version_update.sh path
 
-# build cmake
-mkdir -p ./build
-cd ./build/
-cmake ..
-make pre
-make -j4
-cd ..
-echo "Compilation ends successfully!"
-
 old_path=$1
 echo "------------------start version update package------------------"
 
@@ -29,9 +20,26 @@ else
 fi
 
 old_path=$1'/'
+old_version=""
+if [[ -f $old_path'conf.ini' ]]; then
+    old_version=$(awk -F '=' '/version/ {print$2}' $old_path'conf.ini')
+else
+    old_version=$(awk -F '=' '/version/ {print$2}' $old_path'conf/conf.ini')
+fi
 
 # 新版本路径
 new_path=$(pwd)'/'
+new_version=""
+if [ -f "./conf.ini" ]; then
+    new_version=$(awk -F '=' '/version/ {print$2}' "./conf.ini")
+else
+    new_version=$(awk -F '=' '/version/ {print$2}' "conf/conf.ini")
+fi
+
+if [[ $new_version < $old_version ]]; then
+    echo "the current version shouldn't lower old version !!!"
+    exit
+fi
 
 # -------------------通过gshow获取低版本所有数据库------------------------
 # 进入旧版本路径
@@ -40,6 +48,10 @@ current_time=$(date "+%Y%m%d%H%M%S")
 temp_show_log=$new_path'/show'$current_time.log
 # echo "temp_show_log: $temp_show_log"
 bin/gshow > $temp_show_log
+if [ $? -ne 0 ]; then
+    echo "bin/gshow execution failure !!!"
+    exit
+fi
 temp_database_log=$new_path'/tmp'$current_time.log
 tac "$temp_show_log" | grep '\-\-\-\-\-\-\-\-\-\-\-\-' -m 1 -A 10000 > $temp_database_log
 
@@ -76,13 +88,18 @@ do
     fi
 done < $temp_database_log
 
+conf_path=""
+if [[ $old_version > "1.2" ]]; then
+    conf_path="conf/"
+fi
+
 old_db_home=""
-if [ -f "./init.conf" ];then
-    old_db_home=$(grep -m 1 'db_home' ./init.conf)
+if [ -f $conf_path'init.conf' ];then
+    old_db_home=$(grep -m 1 'db_home' $conf_path'init.conf')
 else
     # echo "init.conf not exist"
-    if [ -f "./conf.ini" ];then
-        old_db_home=$(grep -m 1 'db_home' ./conf.ini)
+    if [ -f $conf_path"conf.ini" ];then
+        old_db_home=$(grep -m 1 'db_home' $conf_path'conf.ini')
     else
         echo "conf.ini not exist"
         exit
@@ -90,7 +107,7 @@ else
 fi
 
 # echo "old_db_home:$old_db_home"
-old_db_home_path=$old_path
+old_db_home_path=""
 if [[ -n $old_db_home ]];
 then
     # 是否注释，默认为数据库为gstore当前目录
@@ -99,13 +116,16 @@ then
     if [[ $is_enable != '#' ]];
     then
         pos=$(echo "${old_db_home}" | awk -F '=' '{print $1}' | wc -c)
-        for i in ${old_db_home:${pos}:${#old_db_home}-${pos}};
-        do
-            if [[ $i != ' ' ]];
-            then
-                old_db_home_path=$old_db_home_path$i
-            fi
-        done
+        temp_d=${old_db_home:${pos}:${#old_db_home}-${pos}}
+        old_db_home_path=$(echo ${temp_d//\"/})
+    fi
+fi
+
+if [[ -z $old_db_home_path ]]; then
+    if [[ $cur_version < "1.3" ]]; then
+        old_db_home_path='.'
+    else
+        old_db_home_path='./dbhome/'
     fi
 fi
 
@@ -113,6 +133,7 @@ if [[ $old_db_home_path = '.' ]];
 then
     old_db_home_path=""
 fi
+old_db_home_path=$old_path$old_db_home_path
 # 删除show.log文件
 rm -rf $temp_show_log
 rm -rf $temp_database_log
@@ -122,18 +143,35 @@ rm -rf $temp_database_log
 # -------------------获取新版本数据库------------------------
 # 切入新版本
 cd $new_path
+# 静态包不进行编译
+if [[ -f 'makefile' ]]; then
+    make clean
+    make pre
+    make -j4
+    echo "Compilation ends successfully!"
+elif [[ -f 'CMakeLists.txt' ]]; then
+    mkdir -p ./build
+    cd ./build/
+    cmake ..
+    make pre
+    make -j4
+    make init
+    cd ..
+    echo "Compilation ends successfully!"
+fi
 new_db_home=""
-if [ -f "./conf/conf.ini" ];then
+if [[ $new_version < "1.3" ]]; then
+    if [ -f "./conf.ini" ];then
+        new_db_home=$(grep -m 1 'db_home' ./conf.ini)
+    else
+        new_db_home=$(grep -m 1 'db_home' ./init.conf)
+    fi
+elif [ -f "./conf/conf.ini" ];then
     new_db_home=$(grep -m 1 'db_home' ./conf/conf.ini)
 fi
 
-if [[ $new_db_home = '.' ]];
-then
-    new_db_home="db_home"
-fi
-
 # echo "new_db_home:$new_db_home"
-new_db_home_path=$new_path
+new_db_home_path=""
 if [[ -n $new_db_home ]];
 then
     # 是否注释，默认为数据库为gstore当前目录
@@ -142,34 +180,34 @@ then
     if [[ $is_enable != '#' ]];
     then
         pos=$(echo "${new_db_home}" | awk -F '=' '{print $1}' | wc -c)
-        for i in ${new_db_home:${pos}:${#new_db_home}-${pos}};
-        do
-            if [[ $i != ' ' ]];
-            then
-                new_db_home_path=$new_db_home_path$i
-            fi
-        done
-    else
-        new_db_home_path=$new_db_home_path"./dbhome/"
+        temp_d=${new_db_home:${pos}:${#new_db_home}-${pos}}
+        new_db_home_path=$(echo ${temp_d//\"/})
     fi
 fi
+
+if [[ -z $new_db_home_path ]]; then
+    if [[ $new_version < "1.3" ]]; then
+        new_db_home_path=""
+    else
+        new_db_home_path="./dbhome/"
+    fi
+fi
+
+new_db_home_path=$new_path$new_db_home_path
 # echo "new_db_home_path:$new_db_home_path"
 
 for database_name in ${database_list[@]}
 do
     # 新版本已经有对应库是否覆盖旧版本
-    old_path=$old_db_home_path$database_name'.db'
-    new_path=$new_db_home_path$database_name'.db'
-    # echo $old_path $new_path
-    if [ -d $new_path ]; then
-        if [ "$database_name" = "system" ]; then
-            continue
-        fi
+    temp_old_path=$old_db_home_path'/'$database_name'.db'
+    temp_new_path=$new_db_home_path'/'$database_name'.db'
+    echo $temp_old_path $temp_new_path
+    if [ -d $temp_new_path ]; then
         read -r -p "the new version already has a $database_name.db, overwrite [Y/n] " input
         case $input in
             [yY][eE][sS]|[yY])
-                rm -rf $new_path
-                cp -rf $old_db_home_path$database_name'.db' $new_db_home_path
+                rm -rf $temp_new_path
+                cp -rf $temp_old_path $new_db_home_path
                 echo "overwrite succeffully"
                 ;;
             [nN][oO]|[nN])
@@ -180,11 +218,36 @@ do
                 ;;
         esac
     else
-         cp -rf $old_db_home_path$database_name'.db' $new_db_home_path
+         cp -rf $temp_old_path $new_db_home_path
     fi
 done
 
-# ------------新版本初始化所有数据库-------------------
+# ------------新版本数据迁移-------------------
+if [[ $new_version < "1.3" ]]; then
+    if [[ $old_db_home_path ]] && [[ $new_db_home_path ]] && [[ $old_db_home_path != $new_db_home_path ]]; then
+        temp_new_path='# db_home=\".\"'
+        replace_path=$(grep "db_home" conf.ini)
+        if [[ -z $replace_path ]]; then
+            replace_path=$(grep "db_home" ini.conf)
+        fi
+        replace_path=$(echo ${replace_path//\"/\\\"})
+        replace_path=$(echo ${replace_path//\//\\\/})
+        path='s/'$temp_new_path'/'$replace_path'/g'
+        sed -i $path conf.ini
+    fi
+fi
+
+if [[ $old_version < "1.3" ]] && [[ $new_version > "1.2" ]] && [[ $old_path'fun/' ]]; then
+    mv ./pfn/cpp ./pfn/cpp.bak
+    cp -rf $old_path'fun/' './pfn/'
+    mv ./pfn/fun ./pfn/cpp/
+elif [[ $old_version < "1.3" ]] && [[ $new_version < "1.3" ]]; then
+    cp -rf $old_path'fun/' ./
+elif [[ $old_version > "1.2" ]] && [[ $new_version > "1.2" ]]; then
+    mv ./pfn/cpp ./pfn/cpp.bak
+    cp -rf $old_path'pfn/cpp/' ./pfn/
+fi
+
 for database_name in ${database_list[@]}
 do
     if [[ $database_name != 'system' ]];
