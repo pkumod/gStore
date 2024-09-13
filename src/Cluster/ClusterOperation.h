4033c4655b0a46611a00b2d82624a708ff8ab25c
wangjian <2606583267@qq.com>
2024-09-13 18:39:36 +0800

    add ClusterOperation

---
+++ src/Cluster/ClusterOperation.h
@@ -0,0 +1,69 @@
+/*
+ * @Author: wangjian 2606583267@qq.com
+ * @Date: 2024-09-13 12:00:00
+ * @LastEditors: wangjian 2606583267@qq.com
+ * @LastEditTime: 2024-09-13 12:00:00
+ * @FilePath: /gstore/src/Cluster/ClusterOperation.h
+ * @Description: 
+ */
+#pragma once
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+namespace cluster
+{
+    // OP(name,string)
+    #define OPERATION_TYPES_MAP(OP)                       \
+        OP(LEADER_HEARTBEAT,                   heartbeat) \
+        OP(LEADER_APPEND,                  appendEntries) \
+        OP(FOLLOWER_REPLY,                         reply) \
+        OP(EXPECTION_CHECK,                        check) \
+        OP(EXPECTION_PREPARE,                    prepare) \
+        OP(EXPECTION_COMMIT,                      commit) 
+
+    enum cluster_operation
+    {
+    #define OP(name, string) name,
+        OPERATION_TYPE_NONE,
+        OPERATION_TYPES_MAP(OP)
+        OPERATION_TYPE_UNDEFINE
+    #undef OP
+    };
+
+    class ClusterOperationHandle
+    {
+    public:
+        static std::string to_str(enum cluster_operation type);
+        static enum cluster_operation to_enum(const std::string &type_str);
+    };
+
+    std::string ClusterOperationHandle::to_str(enum cluster_operation type)
+    {
+        switch (type)
+        {
+    #define OP(name, string) case name: return #string;
+            OPERATION_TYPES_MAP(OP)
+    #undef OP
+            default:
+                return "";
+        }
+    }
+
+    enum cluster_operation ClusterOperationHandle::to_enum(const std::string &type_str)
+    {
+        if (type_str.empty())
+        {
+            return OPERATION_TYPE_NONE;
+        }
+    #define OP(name, string) \
+        if (type_str == #string) { \
+            return name; \
+        }
+        OPERATION_TYPES_MAP(OP)
+    #undef OP
+        return OPERATION_TYPE_UNDEFINE;
+    }
+}
