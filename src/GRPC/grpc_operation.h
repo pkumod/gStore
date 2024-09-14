f8eec9d9f1eba8713e05bef435cf97f704f80348
wangjian <2606583267@qq.com>
2024-09-14 15:16:10 +0800

    update(grpc_operation): modify description

--- src/GRPC/grpc_operation.h
+++ src/GRPC/grpc_operation.h
@@ -4,7 +4,7 @@
  * @LastEditors: wangjian 2606583267@qq.com
  * @LastEditTime: 2022-11-23 15:53:59
  * @FilePath: /gstore/GRPC/grpc_operation.h
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 
  */
 #ifndef _GRPC_OPERATION_H
 #define _GRPC_OPERATION_H
