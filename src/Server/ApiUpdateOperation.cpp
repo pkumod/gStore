cf908a32d657b41d60f7e32c18da2e6842c9fd47
hexuejiang <1632802996@qq.com>
2024-10-21 10:41:12 +0800

    remove backuplog

--- src/Server/ApiUpdateOperation.cpp
+++ src/Server/ApiUpdateOperation.cpp
@@ -196,7 +196,7 @@ namespace server
                         f.open(_db_path + "/success.txt");
                         f.close();
                         // add backup.log
-                        Util::add_backuplog(db_name);
+                        // Util::add_backuplog(db_name);
                         // build response result
                         result = "Import RDF file to database done.";
                         string error_log = _db_path + "/parse_error.log";
@@ -214,7 +214,7 @@ namespace server
                         {
                             Util::remove_path(unz_dir_path);
                         }
-                        Util::add_backuplog(db_name);
+                        // Util::add_backuplog(db_name);
                         apiUtil->update_access_log(0, result, opt_id, 1, success_num, parse_error_num);
                         MessageBuildResponse resp_data;
                         resp_data.StatusCode = StatusOK;
