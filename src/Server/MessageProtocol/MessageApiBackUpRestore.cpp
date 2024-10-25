986201231c9f4234e7e23a8318213bbfcb4ba427
Sunumbrella <1571157272@qq.com>
2024-10-25 11:35:31 +0800

    solve code crash

--- src/Server/MessageProtocol/MessageApiBackUpRestore.cpp
+++ src/Server/MessageProtocol/MessageApiBackUpRestore.cpp
@@ -225,20 +225,4 @@ namespace server
         rJson["filepath"] = this->filepath;
         json_str = rJson.dump();   
     }
-
-    // export
-    MessageExportRequest::MessageExportRequest(const rapidjson::Document& json_data)
-    {
-        this->db_name     = jsonParam(json_data, "db_name");
-		this->db_path = jsonParam(json_data, "db_path");
-        this->compress = jsonBoolParam(json_data, "compress", false);
-    }
-
-    void MessageExportResponse::toJsonString(std::string& json_str)
-    {
-        nlohmann::json rJson;
-        toJson(rJson);
-        rJson["filepath"] = this->filepath;
-        json_str = rJson.dump();
-    }
 }
--- src/Server/MessageProtocol/MessageApiBackUpRestore.h
+++ src/Server/MessageProtocol/MessageApiBackUpRestore.h
@@ -84,19 +84,4 @@ namespace server
         void toJsonString(std::string& json_str) override;
     };
 
-    //export
-    struct MessageExportRequest : public MessageRequest
-    {
-        std::string db_name;
-        std::string db_path;
-        bool compress;
-        MessageExportRequest()=delete;
-        MessageExportRequest(const rapidjson::Document& json_data);
-    };
-
-    struct MessageExportResponse : public MessageResponse
-    {
-        std::string filepath;
-        void toJsonString(std::string& json_str) override;
-    };
 }
