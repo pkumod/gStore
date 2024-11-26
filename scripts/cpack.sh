6f13d2141cfdfad67da3b69e807530357e13a5f5
wangjian <2606583267@qq.com>
2024-11-26 17:43:59 +0800

    update cpack script

--- scripts/cpack.sh
+++ scripts/cpack.sh
@@ -7,8 +7,8 @@ if [ -f /etc/os-release ]; then
     os=$ID
 fi
 architecture=`uname -m`
-version=$(awk -F '=' '/version/ {print$2}' "conf/conf.ini")
-product_name=$(awk -F '=' '/product_name/ {print$2}' "conf/conf.ini")
+version=1.4.2
+product_name=gStore
 product_name_lower=$(echo "$product_name" | tr '[:upper:]' '[:lower:]')
 
 echo "系统: $os"
@@ -20,22 +20,10 @@ echo "start cpack package-------"
 
 # clear build
 echo "start clean build-------"
-cd build/ 
-if [ $? -eq 0 ]; then
-    make clean
-    rm -rf *
-    cmake .. -DCMAKE_BUILD_TYPE=Cpack
-    make -j4
-    cpack
-else
-    echo "build dir not exist"
-    mkdir -p build
-    cd build/
-    make clean
-    rm -rf *
-    cmake .. -DCMAKE_BUILD_TYPE=Cpack
-    make pre
-    make -j4
-    cpack
-fi
-
+rm -rf build
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Cpack
+make pre
+make -j$(nproc)
+cpack
