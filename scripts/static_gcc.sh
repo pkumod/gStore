958da75f51db2787cd802565af13da888c50254d
hexuejiang <1632802996@qq.com>
2025-01-03 09:41:58 +0800

    cmake add static gcc

---
+++ scripts/static_gcc.sh
@@ -0,0 +1,59 @@
+#/bin/bash
+
+# get os info
+os="linux"
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    os=$ID
+fi
+architecture=`uname -m`
+version=1.4
+product_name=gStore
+product_name_lower=$(echo "$product_name" | tr '[:upper:]' '[:lower:]')
+static_pkg_name="$product_name_lower-$version-static-$os-$architecture"
+
+echo "系统: $os"
+echo "架构: $architecture"
+echo "版本: $version"
+echo "产品名称: $product_name"
+echo "安装包名: $static_pkg_name"
+
+echo "start build static package-------"
+
+# clear build
+echo "start clean build-------"
+rm -rf ${static_pkg_name}.tar.gz
+rm -rf ${product_name_lower}
+rm -rf build
+mkdir -p build
+cd build/
+cmake .. -DCMAKE_BUILD_TYPE=StaticGcc
+make pre
+if [ $? -eq 0 ]; then
+    make -j$(nproc)
+    if [ $? -eq 0 ]; then
+        cd ..
+        echo "begin build package-------"
+        mkdir -p ${product_name_lower}/.tmp
+        mkdir -p ${product_name_lower}/conf
+        mkdir -p ${product_name_lower}/scripts
+        mkdir -p ${product_name_lower}/pfn
+        mkdir -p ${product_name_lower}/src/Query/Algorithm
+        mkdir -p ${product_name_lower}/src/Database
+        cp -r conf bin data LICENSE README.md ${product_name_lower}/
+        cp -f conf/* ${product_name_lower}/conf/
+        cp -f scripts/test/api_test.sh ${product_name_lower}/scripts/api_test.sh
+        cp -r pfn ${product_name_lower}/
+        cp -f src/Query/Algorithm/PathQueryHandler.h ${product_name_lower}/src/Query/Algorithm/
+        cp -f src/Database/CSR.h ${product_name_lower}/src/Database/
+        rm -f ${product_name_lower}/bin/.gitignore
+        rm -rf ${product_name_lower}/bin/.gconsole_history
+        tar -czvf ${static_pkg_name}.tar.gz ${product_name_lower}
+        echo "build static package successfully!!!"
+    else
+        echo "build static package make fail!!!"
+    fi
+else
+    echo "build static package cmake fail!!!"
+fi
+
