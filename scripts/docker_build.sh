6f8561a2db15f513d0fc537f562199f74abe2c3d
wangjian <2606583267@qq.com>
2025-04-29 10:53:35 +0800

    update docker build script

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
+# get os info
+os="linux"
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    os=$ID
+fi
+architecture=`uname -m`
+version=1.4
 current_date=$(date +%Y%m%d)
-zip_file="gstore-1.4-image.$current_date.zip"
-tar_file="gstore-1.4.tar"
-img_name="pkumod/gstore:1.4"
+zip_file="gstore-$version-image-$os-$architecture.$current_date.zip"
+tar_file="gstore-$version.tar"
+img_name="pkumod/gstore:$version"
 
 tar -czf gstore.tar.gz api conf 3rdparty data scripts src CMakeLists.txt README.md README_ZH.md LICENSE include/nlohmann/json.hpp
 docker rmi -f "$img_name"
 echo "----------1/5 remove $img_name ok----------"
 docker build -t "$img_name" .
 echo "----------2/5 build $img_name ok----------"
+# 如果第一个参数为zip，则执行
+if [ "$1" = "zip" ]; then
 docker save -o "$tar_file" "$img_name"
-echo "----------3/5 save $img_name to $tar_file ok----------"
-zip -r "$zip_file" "$tar_file"
-echo "----------4/5 zip $tar_file to $zip_file ok----------"
+echo "----------3/5 save $img_name ok----------"
+zip -rq "$zip_file" "$tar_file"
+echo "----------4/5 zip $tar_file ok----------"
+echo "$zip_file"
+else
+echo "----------3/5 skip export docker image----------"
+echo "----------4/5 skip zip docker image----------"
+fi
 rm -f "$tar_file" gstore.tar.gz
 echo "----------5/5 clear tmp file ok----------"
